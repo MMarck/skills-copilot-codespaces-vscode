8fe77ebd976d811ae36769adf911fb7941d3bdf5
MR.Marck <maanmuri@espol.edu.ec>
2023-11-13 13:33:03 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,12 @@
+function skillsMember() {
+    var skills = [
+        {name: "Javascript", level: 1},
+        {name: "Java", level: 3},
+        {name: "C#", level: 2},
+        {name: "Python", level: 1},
+        {name: "C++", level: 1},
+    ]
+    return {
+        skills: skills
+    }
+}
